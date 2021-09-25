f493d13f966ce236138b302899e24b7a5ce22ae9
Onii-Chaan <martinnc@inbox.lv>
2021-09-25 22:22:46 +0300

    Added conan

--- cmake/ExternLibs.cmake
+++ cmake/ExternLibs.cmake
@@ -2,3 +2,29 @@ function(ADDQT5CORE TARGET_NAME)
 	find_package(Qt5 COMPONENTS Core REQUIRED)
 	target_link_libraries(${TARGET_NAME} PRIVATE Qt5::Core)
 endfunction()
+
+function(FETCHSHAREDTYPES)
+	include(FetchContent)
+	FetchContent_Declare(
+	  shared_types
+	  GIT_REPOSITORY https://github.com/Anticarium/Shared_Types.git
+	  GIT_TAG        v1.1.3
+	)
+
+	FetchContent_GetProperties(shared_types)
+	if(NOT shared_types_POPULATED)
+	  FetchContent_Populate(shared_types)
+	endif()
+	ADD_SUBDIRECTORY(${shared_types_SOURCE_DIR} ${shared_types_BINARY_DIR} EXCLUDE_FROM_ALL)
+endfunction()
+
+function(ADDSHAREDTYPES TARGET_NAME)
+	TARGET_INCLUDE_DIRECTORIES(${TARGET_NAME}
+			PRIVATE
+			$<BUILD_INTERFACE:${shared_types_SOURCE_DIR}/src>
+			$<BUILD_INTERFACE:${shared_types_SOURCE_DIR}/include>
+			$<BUILD_INTERFACE:${shared_types_BINARY_DIR}>
+			$<INSTALL_INTERFACE:include>)
+
+	TARGET_LINK_LIBRARIES(${TARGET_NAME} PUBLIC shared_types_include shared_types_lib)
+endfunction()
