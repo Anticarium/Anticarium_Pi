831526294b3ef3be0aeac6321f908a7b8160049f
Onii-Chaan <martinnc@inbox.lv>
2021-09-26 12:38:50 +0300

    Nlohmann json include works

--- apps/server_app/main.cpp
+++ apps/server_app/main.cpp
@@ -1,6 +1,6 @@
 #include <QCoreApplication>
 #include <QDebug>
-
+#include <shared_types/Control.h>
 int main(int argc, char* argv[]) {
     QCoreApplication a(argc, argv);
     qDebug() << "Hello, User!";
