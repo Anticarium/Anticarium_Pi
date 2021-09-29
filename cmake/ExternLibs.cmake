5daa454bf84c945dcb0e40152d61e2b01fc989a4
Onii-Chaan <martinnc@inbox.lv>
2021-09-29 14:38:50 +0300

    Cmake structure for gtests

--- cmake/ExternLibs.cmake
+++ cmake/ExternLibs.cmake
@@ -34,3 +34,14 @@ function(ADDSHAREDTYPES TARGET_NAME)
 
 	TARGET_LINK_LIBRARIES(${TARGET_NAME} PUBLIC shared_types_include shared_types_lib)
 endfunction()
+
+function(ADDGTEST TARGET_NAME)
+        include(FetchContent)
+        FetchContent_Declare(
+                googletest
+                URL https://github.com/google/googletest/archive/2f80c2ba71c0e8922a03b9b855e5b019ad1f7064.zip
+        )
+        set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
+        target_link_libraries(${TARGET_NAME} PRIVATE gtest_main)
+        FetchContent_MakeAvailable(googletest)
+endfunction()
