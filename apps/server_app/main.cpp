72c13c331e43913d76070d04ca156f82268b226d
Onii-Chaan <martinnc@inbox.lv>
2021-10-09 11:57:26 +0300

    Logger in main works

--- apps/server_app/main.cpp
+++ apps/server_app/main.cpp
@@ -1,8 +1,29 @@
 #include <QCoreApplication>
 #include <anticarium_pi/AnticariumManager.h>
 #include <anticarium_pi/config/ApplicationSettings.h>
+#include <spdlog/sinks/daily_file_sink.h>
+#include <spdlog/sinks/rotating_file_sink.h>
+#include <spdlog/sinks/stdout_sinks.h>
+
+#include <spdlog/spdlog.h>
 
 int main(int argc, char* argv[]) {
+    std::shared_ptr<spdlog::sinks::rotating_file_sink_st> rotatingLogger = std::make_shared<spdlog::sinks::rotating_file_sink_st>("logs/mylogfile", 1048576 * 5, 3);
+    std::shared_ptr<spdlog::sinks::daily_file_sink_st> dailyLogger       = std::make_shared<spdlog::sinks::daily_file_sink_st>("logs/mylogfile", 0, 0, false, 10);
+    std::shared_ptr<spdlog::sinks::stdout_sink_st> consoleLogger         = std::make_shared<spdlog::sinks::stdout_sink_st>();
+
+    std::vector<spdlog::sink_ptr> loggerSinks;
+
+    loggerSinks.push_back(rotatingLogger);
+    loggerSinks.push_back(dailyLogger);
+    loggerSinks.push_back(consoleLogger);
+    std::shared_ptr<spdlog::logger> combined_logger = std::make_shared<spdlog::logger>("logger", begin(loggerSinks), end(loggerSinks));
+
+    spdlog::register_logger(combined_logger);
+    spdlog::set_default_logger(combined_logger);
+
+    spdlog::info("Hello, World!");
+
     QCoreApplication a(argc, argv);
     ApplicationSettings::instance(QCoreApplication::applicationDirPath(), QCoreApplication::instance());
     AnticariumManager anticariumManager;
