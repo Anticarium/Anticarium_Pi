dd13e9e0016e36e3d724ce6b33cfcf7b2e70f9d7
MartinsSmirnovs <martinnc@inbox.lv>
2022-06-15 14:47:12 +0300

    Added dynamicSettings tests

--- tests/config/SettingsTests.cpp
+++ tests/config/SettingsTests.cpp
@@ -1,5 +1,7 @@
+#include <QTemporaryFile>
 #include <TestDataDirectory.h>
 #include <config/ApplicationSettings.h>
+#include <config/DynamicSettings.h>
 #include <gtest/gtest.h>
 
 TEST(TestApplicationSettings, TestRead) {
@@ -18,4 +20,24 @@ TEST(TestApplicationSettings, TestRead) {
     EXPECT_EQ(applicationSettings->getFps(), 10);
     EXPECT_EQ(applicationSettings->getLogLevel(), 2);
     EXPECT_EQ(applicationSettings->getUDPThreadsAmount(), 1);
+}
+
+TEST(TestApplicationSettings, TestWrite) {
+    QTemporaryFile file;
+    file.open();
+
+    {
+        QObject p;
+        auto dynamicSettings = DynamicSettings::instance(file.fileName(), &p);
+        dynamicSettings->setP(0.123456789);
+        dynamicSettings->setI(0.94129049012);
+        dynamicSettings->setD(0.2328742142);
+    }
+
+    QObject p;
+    auto dynamicSettings = DynamicSettings::instance(file.fileName(), &p);
+
+    EXPECT_EQ(0.123456789, dynamicSettings->getP());
+    EXPECT_EQ(0.94129049012, dynamicSettings->getI());
+    EXPECT_EQ(0.2328742142, dynamicSettings->getD());
 }
